dbe4ccd7367ad4e89e0ffc39684def2debc5ebaa
SUDEEP <SUDEEP@192.168.0.103>
2022-04-25 16:15:21 +0530

    By Ash25

---
+++ PushPullGithub/src/com/cjc/Hello.java
@@ -0,0 +1,19 @@
+package com.cjc;
+
+public class Hello {
+
+	public void m2()
+	{
+		System.out.println("Hellooo M2");
+	}
+		
+		
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			
+			Hello h=new Hello();
+			h.m2();
+
+	}
+
+}
