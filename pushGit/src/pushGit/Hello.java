ec8e55fc516a45983142d4a5150ea4e4bba34d6d
SUDEEP <SUDEEP@192.168.0.103>
2022-04-25 16:26:58 +0530

    By ASh1-25

---
+++ pushGit/src/pushGit/Hello.java
@@ -0,0 +1,15 @@
+package pushGit;
+
+public class Hello {
+	
+	public void m1()
+	{
+		System.out.println("Helllooo m1");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
