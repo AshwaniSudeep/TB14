e4b06d3624dd4c38d08fe9e26bc2cc6c8efb0efb
SUDEEP <SUDEEP@192.168.0.103>
2022-04-25 20:45:28 +0530

    By Ashwani

--- Git1/src/com/cjc/git1.java
+++ Git1/src/com/cjc/git1.java
@@ -6,6 +6,10 @@ public class git1 {
 	{
 		System.out.println("m1.......A");
 	}
+	public void m2()
+	{
+		System.out.println("m2.......A");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
